8d30cf122c603ee1435b7691b439b036f580a1e1
lan <lan@aladinfun.com>
2023-03-16 15:31:24 +0800

    读prometheus

--- trace/prometheus/query/gocli_test.go
+++ trace/prometheus/query/gocli_test.go
@@ -2,7 +2,9 @@ package query
 
 import (
 	"context"
+	"fmt"
 	"github.com/prometheus/client_golang/api"
+	"github.com/prometheus/common/model"
 	"testing"
 	"time"
 )
@@ -17,11 +19,14 @@ func TestQueryByCli(t *testing.T) {
 	}
 	ctx := context.Background()
 	pmql := NewAPI(cli)
-	val, warn, err := pmql.Query(ctx, "midplat_request_total{}", time.Now())
+	val, warn, err := pmql.Query(ctx, fmt.Sprintf("sum(rate(midplat_request_total{cmd=\"%s\",t_srv_name=\"%s\"}[5m]))by(cmd,t_srv_name)", "LoginV1", "fimsrv"), time.Now())
 	if err != nil {
 		t.Fatalf(err.Error())
 	}
 	t.Log(warn)
-	t.Log(val)
-	
+	vec := val.(model.Vector)
+	for i := 0; i < vec.Len(); i++ {
+		v := vec[i]
+		t.Logf("val = %s \n", v.Value.String())
+	}
 }
